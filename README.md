96d5cd41661a91c5676433a3963de9918f06cced
Aagat <aagatpokhrel@gmail.com>
2023-03-09 13:39:33 +0545

    working but not elastic

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Big-Data-Project
+
+First install Docker
+Setup a database in mongodb atlas
+Also put .env file in backend and put your own MONGO_URI and ELASTIC_URI (elastic search is setup from docker) fields for mongodb and elastic-search path
+Then in terminal if you are runing first time do this otherwise skip this:
+``` py setup.py ```
+Then,
+``` cd frontend ```
+``` npm start ```
+And,
+``` cd backend ```
+``` py app.py ```
+
+and you are good to go.
--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 import os
 import subprocess
-import time
 
-def start_backend():
+def setup_backend():
     os.chdir('backend')
     subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
-    backend = subprocess.Popen(['python', 'app.py'])
-    backend.wait()
 
-def start_frontend():
+def setup_frontend():
     os.chdir('frontend')
     subprocess.run(['npm', 'install'])
-    frontend = subprocess.Popen(['npm', 'start'])
-    frontend.wait()
 
-def start_elastic():
+def setup_elastic():
     os.system('docker pull docker.elastic.co/elasticsearch/elasticsearch:8.6.2')
     os.system('docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.6.2')
 
 if __name__ == '__main__':
-    start_elastic()
-    start_backend()
-    start_frontend()
+    setup_elastic()
+    setup_backend()
+    setup_frontend()
